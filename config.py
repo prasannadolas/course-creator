a8f63de1f617b32bd102db9a65d3fedb8f86afb2
Prasanna Dolas <prasannadolas2683@gmail.com>
2026-07-06 16:54:20 +0530

    updated the key solutions

--- config.py
+++ config.py
@@ -7,9 +7,8 @@ load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
-# UPDATED: Using the 'Lite' model which is more efficient for multiple calls
 MODEL_NAME = "gemini-2.5-flash"
 if not GOOGLE_API_KEY:
     raise ValueError("❌ API Key missing! Check .env file.")
-if not JWT_SECRET_KEY: # <--- ADD THIS SAFETY CHECK
+if not JWT_SECRET_KEY: 
     raise ValueError("❌ JWT_SECRET_KEY missing! Check .env file.")
